2b6d6eb59939a13fead1fe7dda34e1707d9af4d2
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-08-21 14:57:40 -0300

    Ex01 feito

---
+++ Lista16/Lista16/velocidade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista16
+{
+    class velocidade
+    {
+        private double km;
+        private double h;
+        public void SetDis(double km) {this.km = km;}
+        public void SetTempo(double h) {this.h = h;}
+        public double GetDis() {return km;}
+        public double GetTempo() {return h;}
+        public double CalcVelovidade()
+        {
+            return km/h;
+        }
+    }
+}
