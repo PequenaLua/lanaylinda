ea1786f2f01449e248cc95cd01d33d128daad7d0
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-08-26 18:16:54 -0300

    Ex08

---
+++ Lista16/Ex08/Loja.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex08
+{
+    class Loja
+    {
+        private string nome;
+        private Veiculo[] vcls = new Veiculo[15];
+        private int n;
+        public void Inserir(Veiculo v)
+        {
+            if(n < 15)
+            {
+                vcls[n] = v;
+            }
+        }
+        public Veiculo[] Listar()
+        {
+            Veiculo[] vet = new Veiculo[n];
+            Array.Copy(vcls, vet, n);
+            return vet;
+        }
+        public Veiculo[] ListarValor(decimal precoMax)
+        {
+            int g = 0;
+            Veiculo[] vet1 = new Veiculo[15];
+            for(int i = 0; i < n; i++)
+            {
+                if(vcls[i].GetVendido() == false || vcls[i].GetPreco() <= precoMax)
+                {
+                    vet1[g] = vcls[i];
+                    g++;
+                }
+            }
+            Veiculo[] vet2 = new Veiculo[g];
+            Array.Copy(vet1, vet2, g);
+            return vet2;
+        }
+        public decimal Total()
+        {
+            decimal total = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if (vcls[i].GetVendido() == false)
+                {
+                    total += vcls[i].GetPreco();
+                }
+            }
+            return total;
+        }
+    }
+    class Veiculo
+    {
+        private string placa;
+        private string fabricante;
+        private string modelo;
+        private int ano;
+        private decimal preco;
+        private bool vendido;
+        public void SetPlaca(string p) { placa = p; }
+        public void SetFabricante(string f) { fabricante = f; }
+        public void SetModelo(string m) { modelo = m; }
+        public void SetAno(int a) { ano = a; }
+        public void SetPreco(decimal p) { preco = p; }
+        public void SetVendido(bool v) { vendido = v; }
+        public void Vender() {vendido = true;}
+        public bool GetVendido(){return vendido;}
+        public decimal GetPreco() { return preco; }
+    }
+}
---
+++ Lista16/Ex08/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex08
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Loja l = new Loja();
+
+        private void btnInserir(object sender, RoutedEventArgs e)
+        {
+            Veiculo x = new Veiculo();
+            x.SetPlaca(txtPlaca.Text);
+            x.SetFabricante(txtFabricante.Text);
+            x.SetModelo(txtModelo.Text);
+            x.SetAno(int.Parse(txtAno.Text));
+            x.SetPreco(decimal.Parse(txtPreco.Text));
+            bool ver;
+            if (check.IsChecked == true) ver = true;
+            else ver = false;
+            x.SetVendido(ver);
+            l.Inserir(x);
+        }
+
+        private void btnVender(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
