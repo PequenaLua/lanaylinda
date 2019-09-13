dc28cc7e7386bc85d86d21d0807d5a40aebb9295
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-09-13 09:40:02 -0300

    Ex01

--- Lista17_/Lista17_/MainWindow.xaml.cs
+++ Lista17_/Lista17_/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lista17_
+namespace Ex01
 {
     /// <summary>
     /// Interação lógica para MainWindow.xam
@@ -23,6 +23,29 @@ namespace Lista17_
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DisciplinaSemestral x = new DisciplinaSemestral(
+                snome.Text, int.Parse(s1bim.Text), int.Parse(s2bim.Text),
+                int.Parse(sfinal.Text));
+            list.Items.Add(x);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DisciplinaAnual x = new DisciplinaAnual(
+                anome.Text, int.Parse(a1bim.Text), int.Parse(a2bim.Text),
+                int.Parse(a3bim.Text), int.Parse(a4bim.Text),
+                int.Parse(afinal.Text));
+            list.Items.Add(x);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            IMedia x = list.SelectedItem as IMedia;
+            MessageBox.Show(x.GetMedia().ToString());
         }
     }
 }
