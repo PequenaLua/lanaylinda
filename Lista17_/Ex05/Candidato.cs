35d67aca2e88c4e9232e761e2cbfd8f39d0f4c86
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-09-19 21:08:09 -0300

    Ex05

---
+++ Lista17_/Ex05/Candidato.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    class Candidato: IComparable
+    {
+        private string nome;
+        private DateTime nascimento;
+        private int pontos;
+        public Candidato(string n, DateTime nasci, int p)
+        {
+            nome = n;
+            nascimento = nasci;
+            pontos = p;
+        }
+        public override string ToString()
+        {
+            return $"{nome} - {pontos} pontos - Data de nascimento {nascimento:dd/MM/yyyy}";
+        }
+        public int GetPontos() { return pontos; }
+        public int CompareTo(object obj)
+        {
+            Candidato c = obj as Candidato;
+            if (this.pontos > c.pontos) return -1;
+            if (this.pontos < c.pontos) return 1;
+            int s = this.nascimento.CompareTo(c.nascimento);
+            if (s != 0) return s;
+            else return this.nome.CompareTo(c.nome);
+        }
+    }
+}
---
+++ Lista17_/Ex05/Concurso.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    class Concurso
+    {
+        Candidato[] candidatos = new Candidato[20];
+        private int k;
+        public void Inserir(Candidato c)
+        {
+            if(k < 20)
+            candidatos[k++] = c;
+        }
+        public Candidato[] Listar()
+        {
+            Candidato[] novo = new Candidato[k];
+            Array.Copy(candidatos, novo, k);
+            Array.Sort(novo);
+            return novo;
+        }
+        public Candidato[] Classificados()
+        {
+            int t = 0;
+            Candidato[] cl = new Candidato[k];
+            for(int i = 0; i < k; i++)
+            {
+                if(candidatos[i].GetPontos() >= 60) {
+                    cl[t++] = candidatos[i];
+                }
+            }
+            Candidato[] classicados = new Candidato[k];
+            Array.Copy(cl, classicados, t);
+            Array.Sort(classicados);
+            return classicados;
+        }
+    }
+}
