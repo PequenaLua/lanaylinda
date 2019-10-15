85b08dc7fe8e5d88272499e97e08d411cd84b55d
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-10-15 14:21:47 -0300

    Terminando o xaml do Ex08

---
+++ Lista20/Ex08 - WPF/Estoque.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex08___WPF
+{
+    class Estoque
+    {
+        List<Produto> produtos = new List<Produto>();
+        public void Inserir(Produto p) { produtos.Add(p); }
+        public void Excluir(Produto p) { produtos.Add(p); }
+        public Produto[] ListarDescricao()
+        {
+            Produto[] novo = produtos.OrderBy(Produto => Produto.Descricao).ToArray();
+            return novo;
+        }
+        public Produto[] ListarPreco()
+        {
+            Produto[] novo = produtos.OrderByDescending(Produto => Produto.Preco).ToArray();
+            return novo;
+        }
+        public Produto[] Vencidos()
+        {
+            List<Produto> novo = new List<Produto>();
+            foreach (Produto i in produtos) if (i.GetVencido() == true) novo.Add(i);
+            Produto[] vet = novo.ToArray();
+            return vet;
+        }
+        public Produto[] AVencer(int dias)
+        {
+            TimeSpan ts = new TimeSpan(dias, 0, 0, 0);
+            DateTime dt = DateTime.Now;
+            dt += ts;
+            List<Produto> novo = new List<Produto>();
+            foreach (Produto i in produtos)
+            {
+                if (i.Validade <= dt.Date) novo.Add(i);
+            }
+            Produto[] vet = novo.ToArray();
+            return vet;
+        }
+        public Produto[] EstoqueBaixo(int min)
+        {
+            List<Produto> novo = new List<Produto>();
+            foreach (Produto i in produtos) if (i.Qtd <= min) novo.Add(i);
+            Produto[] vet = novo.ToArray();
+            return vet;
+        }
+        public decimal Total()
+        {
+            decimal total = 0;
+            foreach (Produto i in produtos) total += i.Total;
+            return total;
+        }
+    }
+}
---
+++ Lista20/Ex08 - WPF/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex08___WPF
+{
+    class Produto
+    {
+        private string descricao;
+        private decimal preco;
+        private int qtd;
+        private DateTime validade;
+        public string Descricao { get => descricao; }
+        public decimal Preco { get => preco; }
+        public DateTime Validade { get => validade; }
+        public int Qtd { get => qtd; }
+        public decimal Total { get => preco * qtd; }
+        public Produto(string d, decimal p, int q, DateTime v)
+        {
+            descricao = d;
+            preco = p;
+            qtd = q;
+            validade = v;
+        }
+        public bool GetVencido()
+        {
+            DateTime dt = DateTime.Now;
+            if (validade < dt.Date) return true;
+            return false;
+        }
+        public override string ToString()
+        {
+            return $"{descricao} - {preco} R$ - {qtd} - {validade: dd/MM/yyyy}";
+        }
+    }
+}
