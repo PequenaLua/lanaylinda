08dc6c44ff874c5db89a3ba145961325e1376663
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-10-15 13:48:30 -0300

    a

---
+++ Lista20/Ex04/Agenda.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Agenda
+    {
+        private List<Contato> cs = new List<Contato>();
+        public void Inserir(Contato c)
+        {
+            cs.Add(c);
+        }
+        public List<Contato> Listar()
+        {
+            return cs;
+        }
+        public List<Contato> Pesquisar(string inicio)
+        {
+            List<Contato> novo = new List<Contato>();
+            foreach (Contato i in cs)
+            {
+                string priLetra = i.Nome.Substring(0, 1);
+                novo.Add(i);
+            }
+            return novo;
+        }
+    }
+}
---
+++ Lista20/Ex07 - WPF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex07___WPF
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OpenVeiculoWindow(object sender, RoutedEventArgs e)
+        {
+            VeiculoWindow j2 = new VeiculoWindow();
+            j2.Show();
+        }
+        Loja l;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            l = new Loja(nomeLoja.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            lista.ItemsSource = l.ListarPorFabricante();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            lista.ItemsSource = l.ListarPorModelo();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(l.Total().ToString(), "Total");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(l.BuscarPlaca(placa.Text).ToString());
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            lista.ItemsSource = l.BuscarValor(decimal.Parse(valor.Text));
+        }
+    }
+}
