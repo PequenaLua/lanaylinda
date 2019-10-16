c27e90c100793e937882cc24600793149e5e247e
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-10-16 14:16:39 -0300

    12

---
+++ Lista20/Ex03/Pilha2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    class Pilha2<T>
+    {
+        private Queue<T> q1 = new Queue<T>();
+        private Queue<T> q2 = new Queue<T>();
+    }
+}
---
+++ Lista20/Ex07 - WPF/VeiculoWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ex07___WPF
+{
+    /// <summary>
+    /// Lógica interna para VeiculoWindow.xaml
+    /// </summary>
+    public partial class VeiculoWindow : Window
+    {
+        public VeiculoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Veiculo x = new Veiculo(txtPlaca.Text, txtFabricante.Text, txtModelo.Text, int.Parse(txtAno.Text), decimal.Parse(txtPreco.Text));
+            Loja lo = base.
+        }
+    }
+}
