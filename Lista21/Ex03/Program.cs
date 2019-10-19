8b8ad75caf14582c0f7b9b6130179afe55ffb8c2
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-10-19 15:01:33 -0300

    3

--- Lista21/Ex03/Program.cs
+++ Lista21/Ex03/Program.cs
@@ -10,6 +10,22 @@ namespace Ex03
     {
         static void Main(string[] args)
         {
+            try { 
+            Cliente c = new Cliente("Paulo", "paulo@email.com", "988989587");
+            Conta ct = new Conta("31465", 15000);
+            c.Inserir(ct);
+            ct.Sacar(15500);
+            Console.ReadKey();
+            }
+            catch(InversaoSaldoException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.ReadKey();
+            }
+            catch(ArgumentOutOfRangeException erro)
+            {
+                Console.WriteLine(erro.Message);
+            }
         }
     }
 }
